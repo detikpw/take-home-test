53fcc5b822079d4db851a639405e19b4bdda717d
Detik <the2ndflamingheart@gmail.com>
2021-02-12 06:53:00 +0700

    [question-1] return uidToName

--- question-1.js
+++ question-1.js
@@ -1,6 +1,14 @@
 function solution(record) {
   let answer = [];
-  return record
+  const uidToName = record.reduce((uidToNameTemp, message) => {
+    const messageArr = message.split(' ');
+    if (messageArr[0] === 'leaver') return uidToNameTemp
+    return {
+    ...uidToNameTemp,
+    [messageArr[1]]: messageArr[2]
+    }
+  }, {})
+  return uidToName
 }
 
 const exampleRecord = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
