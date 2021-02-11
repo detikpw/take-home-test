5eb92b2869ea51194acc90ddebe27319448eb2e3
Detik <the2ndflamingheart@gmail.com>
2021-02-12 06:32:14 +0700

    Init question-1

---
+++ question-1.js
@@ -0,0 +1,10 @@
+function solution(record) {
+  let answer = [];
+  return record
+}
+
+const exampleRecord = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
+
+console.log('------------------------------------');
+console.log(solution(exampleRecord));
+console.log('------------------------------------');
