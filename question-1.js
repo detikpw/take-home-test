90afb0fa868e8217011a471aad3152dee1a34380
Detik <the2ndflamingheart@gmail.com>
2021-02-12 07:26:32 +0700

    [question-1] Refactor use constant instead value directly

--- question-1.js
+++ question-1.js
@@ -1,13 +1,18 @@
+// Action const
+const LEAVE = "Leave";
+const ENTER = "Enter";
+const CHANGE = "Change";
+
 const messageArrToActivityMessage = (messageArr, uidToName) => {
   switch (messageArr[0]) {
-    case "Enter":
+    case ENTER:
       return `${uidToName[messageArr[1]]} came in`;
 
-    case "Leave":
+    case LEAVE:
       return `${uidToName[messageArr[1]]} has left`;
 
     default:
-      throw Error("Invalid action, should Enter or Leave");
+      throw Error(`Invalid action, should ${ENTER} or ${LEAVE}`);
   }
 };
 
@@ -15,7 +20,7 @@ function solution(record) {
   let answer = [];
   const uidToName = record.reduce((uidToNameTemp, message) => {
     const messageArr = message.split(" ");
-    if (messageArr[0] === "leave") return uidToNameTemp;
+    if (messageArr[0] === LEAVE) return uidToNameTemp;
     return {
       ...uidToNameTemp,
       [messageArr[1]]: messageArr[2],
@@ -23,7 +28,7 @@ function solution(record) {
   }, {});
   answer = record.reduce((messages, message) => {
     const messageArr = message.split(" ");
-    if (messageArr[0] === "Change") return messages;
+    if (messageArr[0] === CHANGE) return messages;
     return [...messages, messageArrToActivityMessage(messageArr, uidToName)];
   }, []);
   return answer;
