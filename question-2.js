727f5944e68690449182bb60056cdd882eb0ed69
Detik <the2ndflamingheart@gmail.com>
2021-02-13 09:58:10 +0700

    [question-2] Get failure rate by stage

--- question-2.js
+++ question-2.js
@@ -1,6 +1,18 @@
 function solution(N, users) {
   let answer = [];
-  return answer
+  const totalPlayerByStages = users.reduce((acc, user) => {
+    if (user === N + 1) return acc
+    return {
+      ...acc,
+      [user]: (acc[user] || 0) + 1
+  }}, {})
+  let remainingPlayer = users.length
+  const failureRateByStages = Object.entries(totalPlayerByStages).map(([stage, totalPlayerinStage]) => {
+    const failureRate = totalPlayerinStage / remainingPlayer
+    remainingPlayer = remainingPlayer - totalPlayerinStage
+    return [Number(stage), failureRate]
+  })
+  return failureRateByStages
 }
 
 exports.default = solution;
