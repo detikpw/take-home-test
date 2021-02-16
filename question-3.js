ab7e77866c19d535d8ee8085bf5f6f077b3c6d82
Detik <the2ndflamingheart@gmail.com>
2021-02-16 09:44:44 +0700

    [question-3] Refactor

--- question-3.js
+++ question-3.js
@@ -2,48 +2,67 @@ function solution(relation) {
   let answer = 0;
 
   // TODO Simplify this structure
-  const relationGroupedByColumn = relation.reduce((acc, currentRow) => 
-    currentRow.map((value, index) => (acc[index] || []).concat(value))
-  , [])
-  let nonUniqueColumnIndexes = []
-  
-  const isColumnUnique = column => (new Set(column)).size === column.length
-  
-  const uniqueColumns = [...relationGroupedByColumn.entries()]
-  // TODO Reduce instead  filter and map
-  .filter(([_key, col], index) => {
-    const isUnique = isColumnUnique(col);
-    if (!isUnique) {
-      nonUniqueColumnIndexes.push(index)
-    }
-    return isUnique;
-  })
-  .map(([key]) => [key.toString()])
+  const relationGroupedByColumn = relation.reduce(
+    (acc, currentRow) =>
+      currentRow.map((value, index) =>
+        (acc[index] || [`COL#${index + 1}`]).concat(value)
+      ),
+    []
+  );
 
-  const newMatrixNonUniqueColumn = nonUniqueColumnIndexes.map(colIndex => [colIndex, ...relationGroupedByColumn[colIndex]])
+  const isColumnUnique = (column) => new Set(column).size === column.length;
+
+  const getUniqueColumnNamesAndNonUniqueColumns = (getCandidateKey) => (
+    { nonUniqueColumns, uniqueColumnNames },
+    currentColumn
+  ) => {
+    if (isColumnUnique(currentColumn)) {
+      return {
+        nonUniqueColumns,
+        uniqueColumnNames: [...uniqueColumnNames, [getCandidateKey(currentColumn)]],
+      };
+    }
+    return {
+      uniqueColumnNames,
+      nonUniqueColumns: [...nonUniqueColumns, currentColumn],
+    };
+  };
+
+  const { uniqueColumnNames, nonUniqueColumns } = relationGroupedByColumn
+    .reduce(getUniqueColumnNamesAndNonUniqueColumns(col => col[0]), {
+      nonUniqueColumns: [],
+      uniqueColumnNames: [],
+    });
   const joinColumns = (columns) => {
-    if (columns.length === 1) return []
-    const [col1, col2, ...restColumns ] = columns
-    return [col1.map((value, index) => `${value} - ${col2[index]}`), ...joinColumns([col1, ...restColumns])]
-  }
+    if (columns.length === 1) return [];
+    const [col1, col2, ...restColumns] = columns;
+    return [
+      col1.map((value, index) => `${value} - ${col2[index]}`),
+      ...joinColumns([col1, ...restColumns]),
+    ];
+  };
 
   const getJoinedColumns = (columns) => {
-    if (columns.length === 1) return []
-    const [_col1, col2, ...restColumns] = columns
-    return [...joinColumns(columns), ...getJoinedColumns([col2, ...restColumns])]
-  }
+    if (columns.length === 1) return [];
+    const [_col1, col2, ...restColumns] = columns;
+    return [
+      ...joinColumns(columns),
+      ...getJoinedColumns([col2, ...restColumns]),
+    ];
+  };
 
-  // TODO Reduce instead  filter and map
-  const relationKeys = getJoinedColumns(newMatrixNonUniqueColumn)
-    .filter(isColumnUnique)
-    .map(col => col[0].split(' - '))
+  const { uniqueColumnNames: relationKeys } = getJoinedColumns(nonUniqueColumns).reduce(
+    getUniqueColumnNamesAndNonUniqueColumns((col) => col[0].split(' - ')),
+    {
+      nonUniqueColumns: [],
+      uniqueColumnNames: [],
+    }
+  );
 
-  const candidateKeys = uniqueColumns.concat(relationKeys)
-
+  const candidateKeys = uniqueColumnNames.concat(...relationKeys)
   console.log('------------------------------------');
   console.log({ candidateKeys });
   console.log('------------------------------------');
-
   answer = candidateKeys.length
   return answer;
 }
