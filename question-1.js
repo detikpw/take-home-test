92094176e65515b6489d22ffdc266211d4a499c7
Detik <the2ndflamingheart@gmail.com>
2021-02-16 21:33:49 +0700

    [question-1] Remove unneeded line

--- question-1.js
+++ question-1.js
@@ -34,16 +34,4 @@ function solution(record) {
   return answer;
 }
 
-const exampleRecord = [
-  "Enter uid1234 Muzi",
-  "Enter uid4567 Prodo",
-  "Leave uid1234",
-  "Enter uid1234 Prodo",
-  "Change uid4567 Ryan",
-];
-
-console.log("------------------------------------");
-console.log(solution(exampleRecord));
-console.log("------------------------------------");
-
 exports.default = solution;
