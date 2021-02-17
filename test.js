bdae1f0b90ed8199fd1886fd5858f4f324498abd
Detik <the2ndflamingheart@gmail.com>
2021-02-17 09:08:48 +0700

    [extra] Add snapshot test

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const test = require("ava");
 const question1Solution = require("./question-1").default;
 const question2Solution = require("./question-2").default;
 const question3Solution = require("./question-3").default;
+const question1Records = require("./question-1-custom-records");
+const question3Relations = require("./question-3-custom-relations");
 
 test("Question-1", (t) => {
   const records = [
@@ -20,6 +22,38 @@ test("Question-1", (t) => {
   t.deepEqual(question1Solution(records), expected);
 });
 
+test("Question-1 Custom 1", (t) => {
+  const records = [
+    "Enter uid1234 Brother",
+    "Enter uid4567 Miyu",
+    "Leave uid1234",
+    "Enter uid1234 Vilucha",
+    "Change uid4567 Nimrook",
+    "Leave uid1234",
+    "Leave uid4567",
+    "Enter uid4567 Wakka",
+    "Enter uid1234 Keepa",
+    "Change uid4567 Letty",
+    "Enter uid7777 Jassu",
+  ];
+  const expected = [
+    "Keepa came in.",
+    "Letty came in.",
+    "Keepa has left.",
+    "Keepa came in.",
+    "Keepa has left.",
+    "Letty has left.",
+    "Letty came in.",
+    "Keepa came in.",
+    "Jassu came in.",
+  ];
+  t.deepEqual(question1Solution(records), expected);
+});
+
+test("Question-1 Snapshot", (t) => {
+  t.snapshot(question1Solution(question1Records));
+});
+
 test("Question-2 Example 1", (t) => {
   const N = 5;
   const users = [2, 1, 2, 6, 2, 4, 3, 3];
@@ -32,6 +66,12 @@ test("Question-2 Example 2", (t) => {
   const users = [4, 4, 4, 4, 4];
   const expected = [4, 1, 2, 3];
   t.deepEqual(question2Solution(N, users), expected);
+});
+
+test("Question-2 Snapshot", (t) => {
+  const N = 118;
+  const users = [106, 27, 94, 59, 96, 83, 43, 19, 93, 97, 102, 90, 52, 20, 71, 45, 52, 15, 5, 34, 90, 36, 51, 31, 57, 12, 111, 13, 44, 19, 106, 49, 85, 99, 105, 9, 97, 39, 112, 79, 108, 12, 107, 82, 30, 72, 118, 8, 110, 71, 20, 77]
+  t.snapshot(question2Solution(N, users));
 });
 
 test("Question-3", (t) => {
@@ -56,4 +96,8 @@ test("Question-3 Custom 1", (t) => {
     ["600", "apeach", "music", "2", "1", "6"],
   ];
   t.deepEqual(question3Solution(relation), 4);
+});
+
+test("Question-3 snapshot", (t) => {
+  t.snapshot(question3Solution(question3Relations));
 });
